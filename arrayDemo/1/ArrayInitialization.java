4edc150764d8143138b4d96afa5099bac5b29ea4
unknown <kiruba.nandhu@gmail.com>
2021-05-19 19:58:38 +0530

    ArrayInitialization Demo

---
+++ arrayDemo/1/ArrayInitialization.java
@@ -0,0 +1,11 @@
+class ArrayInitialization
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{1,2,4,56};
+        int[] no7 = {100,1,5,7};
+        System.out.println(arr);
+        System.out.println(no7);
+        System.out.println(arr.getClass().getName());
+    }
+}
