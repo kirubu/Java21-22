232c11664f89ce0f97aea11d1a25c47855b18342
unknown <kiruba.nandhu@gmail.com>
2021-05-20 20:02:48 +0530

    2DarrayDemo,JaggedArrayDemo

---
+++ arrayDemo/1/PrintAndPrintln.java
@@ -0,0 +1,13 @@
+class PrintAndPrintln
+{
+    public static void main(String[] args)
+    {
+            System.out.print("Line 1");
+            System.out.print("Line 2");
+            System.out.println("\nStatement 1");
+            System.out.println("Statement 1");
+            System.out.print("");
+            System.out.println();
+
+    }
+}
