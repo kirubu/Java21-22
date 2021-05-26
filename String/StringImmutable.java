419a88a3688adda22a58beca46412552ecc95dd4
unknown <kiruba.nandhu@gmail.com>
2021-05-26 19:38:59 +0530

    stringImmutable2

--- String/StringImmutable.java
+++ String/StringImmutable.java
@@ -2,10 +2,12 @@ class StringImmutable
 {
     public static void main(String[] args)
     {
-        String s = "Welcome ";
+        //String s = "Welcome ";
+        String s = new String("Welcome ");
         System.out.println("Before concat: "+s);
         System.out.println("s.concat(Bigboss 5) "+s.concat("Bigboss 5"));
         System.out.println("After concat: "+s);
+        //s = s.concat("Bigboss 5");
         System.out.println("After Assignment: "+s);
     }
 }
